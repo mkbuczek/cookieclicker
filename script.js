c404339e2cbdde9a81623769542704631b467ecc
mkbuczek <buczekkmason@gmail.com>
2025-02-12 14:48:22 -0500

    v2.2 added mines and click text (needs to be fixed)

--- script.js
+++ script.js
@@ -4,11 +4,13 @@ let cookie = document.getElementById("cookie");
 
 let cookieCount = 0;
 let cps = 0;
+let cookiesPerClick = 1;
 
 let buildings = [
     { id: "cursor", basePrice: 15, count: 0, cps: 0.1},
     { id: "grandma", basePrice: 100, count: 0, cps: 1},
-    { id: "farm", basePrice: 1100, count: 0, cps: 8}
+    { id: "farm", basePrice: 1100, count: 0, cps: 8},
+    { id: "mine", basePrice: 12000, count: 0, cps: 47}
 ];
 
 let upgrades = [
@@ -21,10 +23,27 @@ let upgrades = [
 window.onload = loadGame;
 
 //ADDS COOKIE CLICKING
-cookie.addEventListener("click", function() {
-    cookieCount++;
+cookie.addEventListener("click", function(event) {
+    cookieCount += cookiesPerClick;
+    createCookieClickText(event.clientX, event.clientY);
     updateCookieCount()
 });
+
+function createCookieClickText(x, y){
+    let text = document.createElement("span");
+    text.textContent = `+${cookiesPerClick}`;
+    text.classList.add("cookieClickText");
+
+    text.style.left = x + "px";
+    text.style.top = y + "px";
+    text.style.position = "absolute";
+
+    document.body.appendChild(text);
+
+    setTimeout(() => {
+        text.remove();
+    }, 1000);
+}
 
 //ADDS CPS TO COOKIECOUNT
 setInterval(() => {
@@ -38,6 +57,7 @@ function saveGame(){
     let data = {
         cookieCount: cookieCount,
         cps: cps,
+        cookiesPerClick: cookiesPerClick,
         buildings: buildings.map(building => ({ id: building.id, count: building.count }))
     };
 
@@ -107,9 +127,11 @@ function buyUpgrade(upgradeId) {
         switch (upgradeId) {
             case 1:
                 applyUpgrade("cursor", upgrade.multiplier);
+                cookiesPerClick *= 2;
                 break;
             case 2:
                 applyUpgrade("cursor", upgrade.multiplier);
+                cookiesPerClick *= 2;
                 break;
             case 3:
                 applyUpgrade("grandma", upgrade.multiplier);
